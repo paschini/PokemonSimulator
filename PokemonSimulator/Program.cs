935dd2d720684f34cd96f4a59b5f4636a166dc24
Camila Paschini <camila.paschini@gmail.com>
2025-11-11 16:14:31 +0100

    Add project files.

---
+++ PokemonSimulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
